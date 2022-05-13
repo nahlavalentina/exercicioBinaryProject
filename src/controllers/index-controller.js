d2ab77c216ea4d28e15f9fb437439f0d0ddac299
nahlavalentina <nahlavalentinas@gmail.com>
2022-05-13 15:48:15 -0300

    resgatando valor do campo input

--- src/controllers/index-controller.js
+++ src/controllers/index-controller.js
@@ -1,11 +1,23 @@
 // A classe quando for instanciada vai criar um objeto, para não ter que repeti-los.
 
 class IndexController {
-    constructor(){
+    constructor() {
         this.input = '';
     }
 
-    rescueInput(){
+// Get e Set não são necessariamente obrigatórios, porém importantes para readability
 
+    getInput() {
+        return this.input;
+    }
+
+    setInput(newValue) {
+        this.input = newValue;
+    }
+
+// resgata valor do campo de input e atribui o valor através do método setInput
+
+    rescueInput() {
+        this.setInput($('#input-field').val());
     }
 }
